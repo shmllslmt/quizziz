61c3bf7b6db43dacbc18e3d51f86c5ac654dc16b
Shamilla Selamat <shmllslmt@gmail.com>
2023-03-08 12:57:38 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/com/shamilla/quizziz/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.shamilla.quizziz
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  const QuizPage({Key? key}) : super(key: key);
+
+  @override
+  State<QuizPage> createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+          flex: 7,
+          child: Center(
+            child: Text(
+              'This is where the question is going to go.',
+              style: TextStyle(color: Colors.white70, fontSize: 20),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextButton(
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.resolveWith(
+                          (states) => Colors.green),
+                  foregroundColor: MaterialStateProperty.resolveWith(
+                          (states) => Colors.white),
+                  shape: MaterialStateProperty.resolveWith(
+                          (states) => BeveledRectangleBorder())),
+              child: Text('True'),
+              onPressed: () {
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextButton(
+              style: ButtonStyle(
+                  backgroundColor:
+                  MaterialStateProperty.resolveWith((states) => Colors.red),
+                  foregroundColor: MaterialStateProperty.resolveWith(
+                          (states) => Colors.white),
+                  shape: MaterialStateProperty.resolveWith(
+                          (states) => BeveledRectangleBorder())),
+              child: Text('False'),
+              onPressed: () {
+              },
+            ),
+          ),
+        ),
+        //TODO: Add a Row widget here as your scorekeeper
+      ],
+    );
+  }
+}
